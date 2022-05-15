e4a02382ef6cbab3428c76d7587a08f603dae889
jingxia1219 <jingxia1219@gmail.com>
2022-05-14 21:46:36 -0700

    progress bar prev and next both working

--- script.js
+++ script.js
@@ -25,7 +25,16 @@ function update(){
         }
     })
 
-    const actives = document.querySelectorAll('.active')
     progress.style.width = (currentActive-1)/(circles.length-1)*100+'%';
     console.log((currentActive-1)/circles.length*100+'%')
+
+    if(currentActive === 1){
+        prev.disabled = true
+    } else if(currentActive === circles.length){
+        next.disabled = true
+     } else{
+         prev.disabled = false;
+         next.disabled = false;
+     }
 }
+
